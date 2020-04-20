6b97aacd6c225f91499b8c6a7de12ee32227324a
1xdeadman <daniilZakharchenko@gmail.com>
2020-04-21 02:06:58 +1000

    new dir hierarchy

--- modules/modules_math.py
+++ modules/modules_math.py
@@ -3,35 +3,6 @@
 # https://habr.com/ru/post/337260/
 # https://habr.com/ru/post/112953/
 import math
-from colorama import Fore, Style
-
-
-def compare(in_value_1, in_value_2):
-    value_1 = str(in_value_1)
-    value_2 = str(in_value_2)
-    res_1 = ""
-    res_2 = ""
-    len_1 = len(value_1)
-    len_2 = len(value_2)
-    min_len = min(len_1, len_2)
-    color = None
-    for i in range(0, min_len):
-        n = value_1[i]
-        m = value_2[i]
-        if n != m and color != Fore.RED:
-            res_1 += Fore.RED
-            res_2 += Fore.RED
-            color = Fore.RED
-        elif n == m and color != Fore.GREEN:
-            res_1 += Fore.GREEN
-            res_2 += Fore.GREEN
-            color = Fore.GREEN
-        res_1 += n
-        res_2 += m
-
-    print(res_1)
-    print(res_2)
-    print(Style.RESET_ALL)
 
 
 num = 8
@@ -43,13 +14,9 @@ n = 16
 n10 = 1000
 base = 2
 
-print("math.pow:", math.pow(num, power))
+print("math.pow:", math.pow(num, power))  # проблема точности после 16го знака
 print("pow:", pow(num, power))
 print("pow:", pow(num, power, 100))
-elem = math.pow(131, 8)
-compare(int(elem), int(131 ** 8))
-compare(int(math.pow(45245, 12)), int(45245 ** 12))
-compare(int(math.pow(452451, 12)), int(452451 ** 12))
 
 # аналог a%b, но подходит для float
 print("fmod:", math.fmod(5, 3))
