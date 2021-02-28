ef50478bcac8b8cd9a90b5758c449a39681beb8e
1xdeadman <daniilZakharchenko@gmail.com>
2021-03-01 01:03:31 +1000

    - fix example's order

--- 0_basis/3_cycle.py
+++ 0_basis/3_cycle.py
@@ -21,9 +21,12 @@ for i in range(-10, 10, 1):
 for i in range(10):
     print(i, end=' ')
     break
+
+for i in range(10):
+    print(i, end=' ')
+    break
 else:
     print('without break')
-
 
 # циклы while
 val_1 = 1
@@ -34,7 +37,5 @@ while val_1 < 10:
     str_val_1 = str(val_1)
     print(str_val_1 + ": ", "while")  # преобразование числа в строку
 
-for i in range(10):
-    print(i, end=' ')
-    break
+
 
